23f71a606ba7c4439053c65be0cb6fe21f9e5db8
Ahmed Ali <alii403304@gmail.com>
2023-05-02 16:08:32 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+
+import dalleRoutes from './routes/dalle.routes.js';
+
+const PORT = process.env.PORT || 8000;
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limig: "50mb" }))
+
+app.use("/api/v1/dalle", dalleRoutes);
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: "Hello from DALL.E" })
+})
+
+app.listen(PORT, () => console.log('Server has started on port 8080'))
